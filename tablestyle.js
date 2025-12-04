ea922ab1132a7f7f5a2991edcc5dd0c1908ba1cd
Murari Dahal <muraridahal.2024cse@sece.ac.in>
2025-12-04 08:51:42 +0530

    Tables are created

---
+++ tablestyle.js
@@ -0,0 +1,17 @@
+import Styled from "styled-components";
+export const TableStyle = Styled.div`
+    table{
+        cellpadding: 10px;
+        cellspacing: 10px;
+        border: 2px solid black;
+        width: 50%;
+        text-align: center;
+    }
+    th{
+        background-color: grey;
+        color: white;
+    }
+    td{
+        background-color: lightgrey;
+        color: black;}
+`;
