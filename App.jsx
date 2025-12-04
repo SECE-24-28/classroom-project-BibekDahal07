6e8d91d6b52e9dbbe16dea65722dacbc885199a5
Murari Dahal <muraridahal.2024cse@sece.ac.in>
2025-12-04 08:50:47 +0530

    day 3

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import HelloWorld from "./hello-world/comp";
+import RegTable from "./Table/reg-table";
+import RegForm from "./regform/register";
+import TwoBox from "./aptitude-guru/twobox";
+function App() {
+  return <TwoBox />;
+}
+
+export default App;
