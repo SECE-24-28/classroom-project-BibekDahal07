ab07cbb98bb222d8aa05a4349cfcc35370e69262
Murari Dahal <muraridahal.2024cse@sece.ac.in>
2025-12-05 09:07:12 +0530

    our promises

---
+++ our-promise-style.js
@@ -0,0 +1,188 @@
+import styled from "styled-components";
+
+export const OurPromiseStyle = styled.div`
+  .container {
+    padding-block: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    max-width: 1800px;
+    margin: 1rem auto;
+    border-radius: 10px;
+  }
+
+  .heading {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .title {
+    font-weight: 600;
+    font-size: 48px;
+    text-align: center;
+    font-family: "Work Sans", sans-serif;
+    margin: 0;
+  }
+
+  .sub-title {
+    font-size: 18px;
+    text-align: center;
+    margin-block: 8px;
+    font-family: "Work Sans", sans-serif;
+    font-weight: 500;
+    color: rgb(86, 102, 131);
+  }
+
+  .promises {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 2rem;
+  }
+
+  .code-quality {
+    border-top: 4px solid;
+    display: flex;
+    max-width: 380px;
+    gap: 20px;
+    flex-direction: column;
+    border-radius: 0px 0px 10px 10px;
+    padding: 1.5rem;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    border-color: rgb(68, 178, 179);
+  }
+  .code-icon1 {
+    font-size: 28px;
+    color: rgb(68, 178, 179);
+    background: white;
+  }
+
+  .responsive {
+    border-top: 4px solid;
+    display: flex;
+    max-width: 380px;
+    gap: 20px;
+    flex-direction: column;
+    border-radius: 0px 0px 10px 10px;
+    padding: 1.5rem;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    border-color: rgb(239, 175, 75);
+  }
+  .code-icon2 {
+    font-size: 28px;
+    color: rgb(239, 175, 75);
+    background: white;
+  }
+
+  .team {
+    border-top: 4px solid;
+    display: flex;
+    max-width: 380px;
+    gap: 20px;
+    flex-direction: column;
+    border-radius: 0px 0px 10px 10px;
+    padding: 1.5rem;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    border-color: rgb(0, 189, 235);
+  }
+
+  .code-icon3 {
+    font-size: 28px;
+    color: rgb(0, 189, 235);
+    background: white;
+  }
+
+  .turn-arounds {
+    border-top: 4px solid;
+    display: flex;
+    max-width: 380px;
+    gap: 20px;
+    flex-direction: column;
+    border-radius: 0px 0px 10px 10px;
+    padding: 1.5rem;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    border-color: rgb(134, 74, 249);
+  }
+
+  .code-icon4 {
+    font-size: 28px;
+    color: rgb(134, 74, 249);
+    background: white;
+  }
+  .support {
+    border-top: 4px solid;
+    display: flex;
+    max-width: 380px;
+    gap: 20px;
+    flex-direction: column;
+    border-radius: 0px 0px 10px 10px;
+    padding: 1.5rem;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    border-color: rgb(252, 41, 71);
+  }
+  .code-icon5 {
+    font-size: 28px;
+    color: rgb(252, 41, 71);
+    background: white;
+  }
+  .security {
+    border-top: 4px solid;
+    display: flex;
+    max-width: 380px;
+    gap: 20px;
+    flex-direction: column;
+    border-radius: 0px 0px 10px 10px;
+    padding: 1.5rem;
+    background-color: rgb(255, 255, 255);
+    position: relative;
+    border-color: rgb(50, 199, 102);
+  }
+  .code-icon6 {
+    font-size: 28px;
+    color: rgb(50, 199, 102);
+    background: white;
+  }
+
+  h2 {
+    font-family: "Work Sans", sans-serif;
+    font-weight: 600;
+    font-size: 24px;
+    color: rgb(119, 119, 119);
+    margin: 0px;
+  }
+  p {
+    margin: 0px;
+  }
+
+  .sub-heading {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    gap: 17px;
+  }
+
+  .sub-content {
+    font-family: "Work Sans", sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    color: rgb(86, 102, 131);
+  }
+
+  .icon-wrap {
+    display: flex;
+  }
+
+  .code-icon {
+    font-size: 28px;
+    color: rgb(68, 178, 179);
+    background: white;
+  }
+
+}
+`;
